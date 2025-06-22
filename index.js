f7370adb3f998f308415d492afe5c454f4fee46b
Justine <206380233+jjurelvalenzuela@users.noreply.github.com>
2025-06-22 10:15:20 -0500

    Final Output

--- index.js
+++ index.js
@@ -91,3 +91,43 @@ const characters = [
   "?",
   "/",
 ];
+
+let generateEl = document.querySelector("#generate-el");
+let firstResultEl = document.querySelector("#first-result-el");
+let secondResultEl = document.querySelector("#second-result-el");
+let passwordLength = 15;
+
+function generatePassword() {
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+
+  return password;
+}
+
+generateEl.addEventListener("click", function () {
+  const generatedPasswordOne = generatePassword(passwordLength);
+  const generatedPasswordTwo = generatePassword(passwordLength);
+  firstResultEl.textContent = generatedPasswordOne;
+  secondResultEl.textContent = generatedPasswordTwo;
+});
+
+firstResultEl.addEventListener("click", () => {
+  if (firstResultEl.textContent.trim() !== "") {
+    navigator.clipboard
+      .writeText(firstResultEl.textContent)
+      .then(() => alert("First password copied!"))
+      .catch((err) => console.error("Failed to copy: ", err));
+  }
+});
+
+secondResultEl.addEventListener("click", () => {
+  if (secondResultEl.textContent.trim() !== "") {
+    navigator.clipboard
+      .writeText(secondResultEl.textContent)
+      .then(() => alert("Second password copied!"))
+      .catch((err) => console.error("Failed to copy: ", err));
+  }
+});
